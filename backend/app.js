6032bb4ced2cb7920a729cc213312fcf75a5b96e
Natalie Kalinkina <ms.nataly.r@yandex.ru>
2023-11-07 15:44:10 +0400

    ...

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const { celebrate, Joi, errors } = require('celebrate');
 const cors = require('cors');
 
-const { PORT = 3000, MONGO_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
+const { PORT = 3008, MONGO_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
 const app = express();
 app.use(cors());
