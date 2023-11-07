a42abd5d53f77b80cc47a55844e4461ac6bff773
Natalie Kalinkina <ms.nataly.r@yandex.ru>
2023-11-07 16:06:54 +0400

    Revert "исправить адрес апи"
    This reverts commit f5c4914bbfb70d43aec70fd46925480a89fb4334.
    
    исправить ошибочный коммит

--- frontend/src/utils/Api.js
+++ frontend/src/utils/Api.js
@@ -119,5 +119,5 @@ class Api {
 }
 
 export const api = new Api({
-  url: 'https://nataliekalinkina.mesto.nomoredomainsmonster.ru/api'  
+  url: 'https://nataliekalinkina.mesto.nomoredomainsmonster.ru/api/'  
 });
