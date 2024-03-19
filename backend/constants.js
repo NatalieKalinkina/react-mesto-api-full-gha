bfd98e7001d86210f0b4d5ee58933c81080721cb
Natalie Kalinkina <ms.nataly.r@yandex.ru>
2024-03-19 17:25:23 +0400

    add versel.json

--- backend/constants.js
+++ backend/constants.js
@@ -1,11 +1,11 @@
 const BAD_REQUEST = 400;
+const UNAUTHORIZED = 401;
+const FORBIDDEN = 403;
 const NOT_FOUND = 404;
+const CONFLICT = 409;
 const SERVER_ERROR = 500;
 const OK = 200;
 const CREATED = 201;
-const UNAUTHORIZED = 401;
-const CONFLICT = 409;
-const FORBIDDEN = 403;
 
 // eslint-disable-next-line
 const URL_REGEXP = /^https?\:\/\/[0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*(:(0-9)*)*(\/?)([a-zA-Z0-9\-\.\?\,\'\/\\\+&amp;%\$#_]*)?$/im;
